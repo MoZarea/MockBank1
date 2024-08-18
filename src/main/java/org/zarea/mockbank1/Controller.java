9bfc81bd54bd3c95d0ee7a660ac2e80b2f1c08b5
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-18 23:09:52 +0300

    init

--- src/main/java/org/zarea/mockbank1/Controller.java
+++ src/main/java/org/zarea/mockbank1/Controller.java
@@ -2,14 +2,20 @@ package org.zarea.mockbank1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    int balance = 30000;
     @PostMapping("/balance")
-    public Response<BalanceResponse> balance() {
+    public Response<BalanceResponse> balance(@RequestHeader String token) {
+        System.out.println(token);
+        if (!token.equals("BOE-0112-XgF0")) {
+            throw new IllegalArgumentException("Invalid token");
+        }
         BalanceResponse balanceResponse = new BalanceResponse(
-                "30000"
+                String.valueOf(balance)
         );
         return new Response<>(200,"done",balanceResponse);
     }
